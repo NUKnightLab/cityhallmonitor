7656326b062d7194b129312d2c4f793c533f6c91
Jennifer Wilson <jenny@jyw-pro.local>
2015-08-05 12:45:56 -0500

    Add data=true to DC search URL; document view in progress

--- cityhallmonitor/views.py
+++ cityhallmonitor/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from cityhallmonitor.models import Matter, MatterAttachment
 
 
 def search(request):
     return render(request, 'search.html', context={})
+    
+    
+def documents(request, id):
+    """View MatterAttachment"""
+    try:    
+        matter_attachment = MatterAttachment.objects.get(pk=id) 
+
+        return render(request, 'documents.html', context={
+            'matter_attachment': matter_attachment
+        })
+    except Exception as e:
+        return render(request, 'documents.html', context={
+            'error': str(e)
+        })

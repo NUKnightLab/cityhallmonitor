0091c41d8dd064b40ccf35d4d24c01ae4438f028
jywsn <jennifer.y.wilson@northwestern.edu>
2015-11-25 11:23:36 -0600

    Set sender in signal handlers

--- cityhallmonitor/signals/handlers.py
+++ cityhallmonitor/signals/handlers.py
@@ -1,23 +1,20 @@
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from cityhallmonitor.models import DirtyFieldsModel
 
 
-@receiver(pre_save)
+@receiver(pre_save, sender=DirtyFieldsModel)
 def handle_pre_save(sender, instance, *args, **kwargs):
-    """
-    Set updated_at timestamp if model is actually dirty
-    """
+    """Set updated_at timestamp if model is actually dirty"""
     if hasattr(sender, 'is_dirty'):
         if instance.is_dirty():
             instance.updated_at = timezone.now()
     
 
-@receiver(post_save)
+@receiver(post_save, sender=DirtyFieldsModel)
 def handle_post_save(sender, instance, **kwargs):
-    """
-    Reset dirty state
-    """
+    """Reset dirty state"""
     if hasattr(sender, 'reset_state'):
         instance.reset_state()
     

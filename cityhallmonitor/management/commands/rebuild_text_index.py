ad04cf539a689af9609d8c7eea925e2fcbc0fa70
jywsn <jennifer.y.wilson@northwestern.edu>
2015-12-14 15:49:50 -0600

    Add limit and where options to rebuild_text_index

--- cityhallmonitor/management/commands/rebuild_text_index.py
+++ cityhallmonitor/management/commands/rebuild_text_index.py
@@ -7,12 +7,29 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'For each document, force an update of its related fields and its postgres text index'
 
-    logger.info('Rebuilding text index')
-    
+    def add_arguments(self, parser):
+        parser.add_argument('--limit', type=int, 
+            help='Process up to LIMIT documents')
+        parser.add_argument('--where', 
+            help='WHERE condition to filter documents')
+
     def handle(self, *args, **options):
-        for i,d in enumerate(Document.objects.all()):
+        logger.info(
+            'Rebuilding text index, limit=%(limit)s, where="%(where)s"' \
+            % options)
+        
+        if options['where']:
+            qs = Document.objects.extra(where=[options['where']])
+        else:
+            qs = Document.objects.all()
+            
+        if options['limit']:
+            qs = qs[:options['limit']]
+        
+        for i,d in enumerate(qs, start=1):
             d._set_dependent_fields()
             d.save(update_text=True)
             if i % 1000 == 0:
                 logger.debug("Processed %i documents" % i)
-        logger.info('Done\n')
+        
+        logger.info('Done, processed %d documents\n' % i)

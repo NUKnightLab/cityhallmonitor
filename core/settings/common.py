147caa4a7ba24390a5577126b71406344e4ef1d0
Scott Bradley <scottbradley.mail@gmail.com>
2016-06-21 17:17:03 -0500

    back to a single base settings file called common

--- core/settings/common.py
+++ core/settings/common.py
@@ -21,12 +21,68 @@ from os.path import abspath, dirname, join
 from os import environ as env
 
 PROJECT_NAME = env['PROJECT_NAME']
+CORE_ROOT = dirname(dirname(abspath(__file__)))
+PROJECT_ROOT = dirname(CORE_ROOT)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'cityhallmonitor',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+# databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': env['DB_ENGINE__DEFAULT'],
+        'NAME': env['DB_NAME__DEFAULT'],
+        'USER': env['DB_USER__DEFAULT'],
+        'PASSWORD': env['DB_PASSWORD__DEFAULT'],
+        'HOST': env['DB_HOST__DEFAULT'],
+        'PORT': env['DB_PORT__DEFAULT']
+    }
+}
+
+# DocumentCloud
+
+DOCUMENT_CLOUD_USERNAME = env['DOCUMENT_CLOUD_USERNAME']
+DOCUMENT_CLOUD_PASSWORD = env['DOCUMENT_CLOUD_PASSWORD']
+DOCUMENT_CLOUD_ACCOUNT = env['DOCUMENT_CLOUD_ACCOUNT']
+DOCUMENT_CLOUD_PROJECT = env['DOCUMENT_CLOUD_PROJECT']
+
+# email: used for sending subscription-related emails
+
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'knightlab.cityhallmonitor@gmail.com'
+EMAIL_HOST_USER = env['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = env['EMAIL_HOST_PASSWORD']
+# Domain for links sent in emails from management commands
+DOMAIN_URL = 'https://cityhallmonitor.knightlab.com'
+
+
+# ~~ everything below here is pretty standard and should not need to be edited ~~
+
 ADMINS = (
     ('Knight Lab', 'knightlab@northwestern.edu'),
 )
 MANAGERS = ADMINS
-CORE_ROOT = dirname(dirname(abspath(__file__)))
-PROJECT_ROOT = dirname(CORE_ROOT)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Avoid changing DEBUG here. Instead use the --debug manage.py flag, or
--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,4 +1,4 @@
-from .project import *
+from .common import *
 
 # This is an override of DEBUG in common.py which is set to default to False
 # Do not do this in deployment. Instead, set DJANGO_DEBUG=True in the

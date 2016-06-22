cafaca3be4e74114f97afce24aa30804bb65daf3
Scott Bradley <scottbradley.mail@gmail.com>
2016-06-22 13:24:15 -0500

    replaces django_manage.sh with more general env_run.sh

--- django_manage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# setup the environment and call manage.py
-. ~/env/cityhallmonitor/bin/activate
-. ./env.sh
-./manage.py $@
---
+++ env_run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# setup the environment and call whatever was passed in
+. ~/env/cityhallmonitor/bin/activate
+. ./env.sh
+./$@
